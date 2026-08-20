c770ec75abad8ccbece6ab6a9040d705357731fb
technoxie <1250623166@qq.com>
2026-08-20 17:07:02 +0800

    不需要git跟踪

--- isuzu-UI-Vue3/dist-admin/static/js/index-CSKx3hXm.js
+++
@@ -1,1 +0,0 @@
-import{_ as n}from"./index-BYDKR2dH.js";import{r as o,o as t,l,J as p,j as e}from"./index-baaCChab.js";const f={__name:"index",setup(u){const r=o("/prod-api/swagger-ui/index.html");return(c,s)=>(t(),l(e(n),{src:e(r),"onUpdate:src":s[0]||(s[0]=a=>p(r)?r.value=a:null)},null,8,["src"]))}};export{f as default};
